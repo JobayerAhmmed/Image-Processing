74da9d3a4ca6be5acd4e0b4595b4b447f1bdd1f3
Jobayer Ahmmed <jobayer.ahmmed@gmail.com>
2016-08-10 17:12:37 +0600

    removed comment from main method

--- src/image/Main.java
+++ src/image/Main.java
@@ -9,10 +9,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
 //        CornerDetection.detectCorner();
         LetterSeparation.separateLetter();
-//        Stack stack = new Stack();
-//        stack.push(5);
-//        int p = stack.pop();
-//        System.out.println(stack.pop());
-//        System.out.println(stack.empty());
+
     }
 }
